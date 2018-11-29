3da6bf501a4afa0297ca710efafa7ae173690310
couyang <calvincouyang@gmail.com>
2018-11-28 16:27:30 -0800

    Update multiplication.js

--- multiplication.js
+++ multiplication.js
@@ -1,19 +1,16 @@
-function mul(a) {
-    return function (b) {
-        if (typeof b !== 'undefined') {
-            a = a * b;
-            return (function fn(c) {
-                if (typeof c !== 'undefined') {
-                    a = a * c;
-                    return fn();
-                } else {
-                    return a;
-                }
-            });
-        } else {
-            return a;
-        }
-    };
+function mul(n) {
+    times = n;
+    const proxy = new Proxy(function a () {}, {
+        get (obj, key) {
+            return () => times;
+        },
+        apply (receiver, ...args) {
+        times *= args[1][0];
+        return proxy;
+    },
+    });
+    return proxy;
 }
+
 
 console.log(mul(2)(4)(5));
